25426675553c0bc4ee391f697745019ea0c221ad
Glazelf <gideon.steenbergen@gmail.com>
2022-08-16 17:56:07 +0200

    remove website-only resources

--- src/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$WEB_ROOT = '/pvpoke-unite/src/';
-$WEB_HOST = 'http://' . $_SERVER['HTTP_HOST'] . $WEB_ROOT;
-$UA = 'UA-131578527-2';
-?>
--- src/js/team/Lane.js
+++
@@ -1,9 +0,0 @@
-// Functionality for lane object
-
-function Lane(id, str, n){
-	let self = this;
-	let gm = GameMaster.getInstance();
-	self.laneId = id;
-	self.stringId = str;
-	self.cap = n;
-}
